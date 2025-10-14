972a7a81f710614479511332d54e7ebd16885b33
David Wagner <davidsamuel.wagner@gmail.com>
2025-10-14 17:53:34 +0200

    moved spring boot dir to correct place

---
+++ webapp/src/test/java/edu/fra/uas/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
