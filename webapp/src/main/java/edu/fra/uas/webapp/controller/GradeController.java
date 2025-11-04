13815853bc129fe9696eb0bee4691655e33e3d4e
David Wagner <davidsamuel.wagner@gmail.com>
2025-11-04 13:59:39 +0100

    uebung 3-Noten

--- webapp/src/main/java/edu/fra/uas/webapp/controller/GradeController.java
+++ webapp/src/main/java/edu/fra/uas/webapp/controller/GradeController.java
@@ -1,20 +1,41 @@
 package edu.fra.uas.webapp.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 import edu.fra.uas.webapp.service.*;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+
 
 import java.util.*;
 
-@Component
+@Controller
 public class GradeController {
+    private final Logger log = LoggerFactory.getLogger(GradeController.class);
     @Autowired 
     private GradingService gradingService;
     
     public double calculateAvg(){
         return gradingService.calculateAvgerage();
+    }
+
+    @RequestMapping(value={"/list"},method = RequestMethod.GET)
+    public String listGrades(Model model){
+        log.debug("list() is called");
+        model.addAttribute("grades", gradingService.getGrades());
+        return "notenView.html";
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<h1>Willkommen zur Notenberechnung</h1>";
     }
 }
 
